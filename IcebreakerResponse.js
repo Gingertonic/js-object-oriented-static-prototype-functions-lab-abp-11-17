86bc1daaf7979d71f416c6483b7776691b2c37f4
Beth Schofield <bethmschofield@gmail.com>
2017-11-14 22:43:51 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+user.BatchCreate = function(array) {
+  return array.map(function(users){
+    return new IcebreakerResponse(users[0], users[1])
+  })
+}
